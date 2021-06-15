1328670337f6fb816237997b65c168d7c2ed0a2d
joonaramo <joona00ramo@gmail.com>
2021-06-15 12:14:11 +0300

    10.6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, StyleSheet, Pressable } from 'react-native';
+import { Link } from 'react-router-native';
 import Text from './Text';
 import Constants from 'expo-constants';
 import theme from '../theme';
@@ -10,25 +11,36 @@ const styles = StyleSheet.create({
     paddingBottom: Constants.statusBarHeight,
     backgroundColor: theme.colors.appBarBg,
     display: 'flex',
+    flexDirection: 'row',
   },
-  text: {
+  tab: {
     marginLeft: 12,
     top: Constants.statusBarHeight / 2,
+    paddingVertical: 4,
   },
 });
 
 const AppBar = () => {
   return (
     <View style={styles.container}>
-      <Pressable>
-        <Text
-          color='white'
-          fontSize='subheading'
-          fontWeight='bold'
-          style={styles.text}
-        >
-          Repositories
-        </Text>
+      <Pressable style={styles.tab}>
+        <Link to='/'>
+          <Text color='white' fontSize='subheading' fontWeight='bold'>
+            Repositories
+          </Text>
+        </Link>
+      </Pressable>
+      <Pressable style={styles.tab}>
+        <Link to='/signin'>
+          <Text
+            color='white'
+            fontSize='subheading'
+            fontWeight='bold'
+            style={styles.text}
+          >
+            Sign In
+          </Text>
+        </Link>
       </Pressable>
     </View>
   );
